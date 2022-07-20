699f9e13510f8051ba1eac31e83114850ade29ae
Fábio dos Santos <fabio.sfs@gmail.com>
2022-07-20 19:29:16 -0300

    Criação do repositório.

---
+++ Client.Repository/ClientContext/Repositories/ClientRepository.cs
@@ -0,0 +1,32 @@
+﻿using clientContext = Client.Domain.ClientContext;
+
+namespace Client.Repository.ClientContext.Repositories
+{
+    public class ClientRepository : clientContext.IClientRepository
+    {
+        public async Task<IEnumerable<clientContext.Client>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<clientContext.Client> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<clientContext.Client> InsertAsync(clientContext.Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<clientContext.Client> UpdateAsync(clientContext.Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
